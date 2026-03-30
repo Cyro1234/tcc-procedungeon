3b8694470ca75b2e4738324e468327b98f22dfb3
guthox <gutho.san@gmail.com>
2026-03-30 13:25:25 -0300

    Fix Direction2D duplicado

--- Assets/_Scripts/ScriptsProcedural.cs
+++ Assets/_Scripts/ScriptsProcedural.cs
@@ -13,28 +13,10 @@ public static class ScriptsProcedural
 
         for(int i = 0; i < walkLenght; i++)
         {
-            var newPosition = previousPosition + Direction2D.GetRandomDirection();
+            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
             path.Add(newPosition);
             previousPosition = newPosition;
         }
         return path;
-    }
-}
-
-public class Direction2D
-{
-    public static List<Vector2Int> cardinalDirectionList = new List<Vector2Int>
-    {
-        new Vector2Int(0, 1), // cima
-        new Vector2Int(1, 0), // direita
-        new Vector2Int(0, -1), // baixo
-        new Vector2Int(-1, 0), // esquerda
-
-    };
-
-    // escolhe aleatoriamente pra onde vai andar
-    public static Vector2Int GetRandomDirection()
-    {
-        return cardinalDirectionList[Random.Range(0, cardinalDirectionList.Count)];
     }
 }
