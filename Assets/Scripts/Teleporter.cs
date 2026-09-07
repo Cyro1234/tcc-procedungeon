e06a04793be8d635ffe06fbb3074af7b596c13c6
lilsiru <47306164+Cyro1234@users.noreply.github.com>
2026-09-07 01:56:48 -0300

    teleporter direto sem camera lock

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -1,20 +1,45 @@
 using UnityEngine;
+using Unity.Cinemachine;
+using System.Collections;
 
 public class Teleporter : MonoBehaviour
 {
     private bool used = false;
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    public CinemachineFollow cinemachineFollow;
+
+    private void Start()
+    {   
+        CinemachineCamera cam = FindAnyObjectByType<CinemachineCamera>();
+        if (cam != null)
+        {
+            cinemachineFollow = cam.GetComponent<CinemachineFollow>();
+        }
+    }
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
 
         // Se a escada ja foi usada, nao faz nada para evitar criar multiplas dungeons em uma escada
-        if (used) return;
+        if (used) yield break;
 
         // Se quem entrou na colisao foi o jogador
         if (collision.CompareTag("Player"))
         {
             used = true;
 
+            if (cinemachineFollow != null)
+            {
+                cinemachineFollow.TrackerSettings.PositionDamping = Vector3.zero;
+            }
+
             collision.transform.position = new Vector2(1000, 0);
+
+            yield return new WaitForEndOfFrame();
+
+            if (cinemachineFollow != null)
+            {
+                cinemachineFollow.TrackerSettings.PositionDamping = new Vector3(1f, 1f, 1f);
+            }
         }
     }
 }
